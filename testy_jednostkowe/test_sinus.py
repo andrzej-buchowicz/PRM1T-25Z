61d7592bf94be9ed6111df6c4971e83b616a8de9
Andrzej Buchowicz <andrzej.buchowicz@gmail.com>
2026-01-15 16:06:52 +0100

    unit test 2

--- testy_jednostkowe/test_sinus.py
+++ testy_jednostkowe/test_sinus.py
@@ -1,4 +1,5 @@
 import unittest
+import math
 
 from sinus import sinus
 
@@ -6,6 +7,8 @@ from sinus import sinus
 class SinusTestCase(unittest.TestCase):
     def test_poprawny_wynik(self):
         self.assertEqual(sinus(0.), 0.)
+        self.assertEqual(sinus(math.pi / 2), 1.0)
+        self.assertEqual(sinus(math.pi / 4), math.sqrt(2.) / 2)
 
 
 if __name__ == '__main__':
